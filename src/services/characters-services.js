7b83d49b97409ec4cc5e864b685eb8c2f6563d3f
Théotim ALBERTEAU <sunshouwu@outlook.com>
2026-04-27 13:57:11 +0200

    up : working /characters route | idaocharacters

--- src/services/characters-services.js
+++ src/services/characters-services.js
@@ -1,8 +1,8 @@
 const DAOFactory = require('../dao/dao-factory');
 
 module.exports = {
-    find_all_characters: async () => {
-        const allCharacters = await DAOFactory.getDAOCharacter.selectAll();
+    getAll: async () => {
+        const allCharacters = await DAOFactory.getDAOCharacter().selectAll();
         return allCharacters;
     }
 }
