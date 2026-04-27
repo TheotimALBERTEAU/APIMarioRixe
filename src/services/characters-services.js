63ac2b481160974eef1572805e6a5ec480333dda
Théotim ALBERTEAU <sunshouwu@outlook.com>
2026-04-27 14:30:49 +0200

    feat : /:id route | CharactersFeature

--- src/services/characters-services.js
+++ src/services/characters-services.js
@@ -4,5 +4,10 @@ module.exports = {
     getAll: async () => {
         const allCharacters = await DAOFactory.getDAOCharacter().selectAll();
         return allCharacters;
+    },
+
+    getOneById: async (id) => {
+        const character = await DAOFactory.getDAOCharacter().selectById(id);
+        return character;
     }
 }
